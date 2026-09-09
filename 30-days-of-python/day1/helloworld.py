462f3eb7d61fe932889bf998cabf210ab5e8bf73
yahya maania <zalambiaaa@gmail.com>
2026-09-09 23:39:48 +0100

    Add files via upload

---
+++ 30-days-of-python/day1/helloworld.py
@@ -0,0 +1,22 @@
+#1
+print("im using the python virsion 3.11.4")
+#2
+print(3+4)
+print(3-4)
+print(3*4)
+print(3/4)
+print(3**4)
+print(3%4)
+print(3//4)
+#3
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(["apple", "banana", "cherry "]))
+print(type("yahya"))
+print(type("mannai"))
+print(type("tunisia"))
+
+print("question number 3's answer is :")
+print(" the Euclidean distance between the two points (2,3) and (10,8) is : ")
+print((((10-2)**2)+((8-3)**2))**0.5)
