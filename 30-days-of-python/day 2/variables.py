36a6ccc03d43322cf32f43632ae0f8c046cbd5cd
yahya maania <zalambiaaa@gmail.com>
2026-09-10 22:10:38 +0100

    Add files via upload

---
+++ 30-days-of-python/day 2/variables.py
@@ -0,0 +1,48 @@
+#day 2: 30 days of python
+first_name="yahya"
+last_name="mannai"
+full_name=first_name+" "+last_name
+country="tunisia"
+city="kef"
+age=20 
+year=2026
+is_married=True
+is_true=True
+is_light_on=False
+status="happy"
+height=1.80
+weight=65
+
+print(type(first_name))
+print(type(last_name))
+print(type(full_name))
+print(type(country))
+print(type(city))
+print(type(age))
+print(type(year))
+print(type(is_married))
+print(type(is_true))
+print(type(is_light_on))
+print(type(status))
+print(type(height))
+print(type(weight))
+
+print(len(first_name))
+print(len(last_name)>len(first_name))
+num_one=5
+num_two=4
+total=num_one+num_two
+diff=num_one-num_two
+product=num_one*num_two
+division=num_one/num_two
+remainder=num_one%num_two
+exp=num_one**num_two
+floor_division=num_one//num_two
+area_of_circle=3.14*30**2
+circum_of_circle=2*3.14*30
+radius= int(input("enter the radius of a circle: "))
+area_of_circle=3.14*radius**2
+name=input("enter your name: ")
+last_name=input("enter your last name: ")
+country=input("enter your country: ")
+age=int(input("enter your age: "))
