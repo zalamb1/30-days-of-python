13001d1b5867d0b9dfc9eb761506ba4d8c01fdc5
yahya maania <zalambiaaa@gmail.com>
2026-09-11 22:08:23 +0100

    Add files via upload

---
+++ day 3/operators.py
@@ -0,0 +1,60 @@
+age = 20
+height = 1.82  
+cmplx = 1 + 2j
+#area of triangle
+b=int(input("Enter a base: ")) 
+h=int(input("Enter a height: "))
+print("Area of triangle is: ", 0.5 * b * h)
+#perimeter of triangle
+a=int(input("Enter side a: "))
+b=int(input("Enter side b: "))
+c=int(input("Enter side c: "))
+print("the perimeter of triangle is: ", a + b + c)
+#area of rectangle
+l=int(input("Enter length: "))
+w=int(input("Enter width: "))
+print("Area of rectangle is: ", l * w)
+print("the perimeter of rectangle is: ", 2 * (l + w))
+#area of circle
+r=int(input("Enter radius: "))
+pi=3.14
+print("Area of circle is: ", pi * r * r)
+#the slop thing i studied maath in franche i dont know what it means XD i didnt finish it sorry
+
+print("The euclidean distance is: ", ((2-6)*2+(2-10)*2)**0.5)
+
+
+#words
+p=len("python")
+d=len("dragon")
+p!=d
+if "on" in "python" and "on" in "dragon":
+    print("True")
+else:
+    print("False")
+
+word="jagon"
+t="i hope this is not full of jagon"
+if "jagon" in t:
+    print("True")
+else:
+    print("False")
+print(not("on" in "python" and "on" in "dragon"))
+n=float(len("python"))
+ch=str(n)
+print(ch)
+print(n)
+#math
+print(7//3==int(2.7))
+print(type("10")==type(10))
+print(int(9.8)==10)
+hours=int(input("Enter hours: "))
+rate=int(input("Enter rate per hour: "))
+print ("Your weekly earning is: ", hours * rate)
+years=int(input("Enter number of years you have lived: "))
+print("You have lived for ", years * 365 * 24 * 60 * 60, "seconds.")
+#table
+for i in range(1,6):
+    print(i,i**0,i**1,i**2,i**3)
+
+        
