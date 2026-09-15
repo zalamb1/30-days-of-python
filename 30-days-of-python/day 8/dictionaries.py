201d83dbd1d600ecaf8e48e9beb1e48e366e52f2
yahya maania <zalambiaaa@gmail.com>
2026-09-15 23:37:31 +0100

    Add files via upload

---
+++ 30-days-of-python/day 8/dictionaries.py
@@ -0,0 +1,17 @@
+dog={}
+dog['name']='max'
+dog['color']="black"
+dog['breed']="german"
+dog['legs']=4
+dog['age']=3
+print(dog)
+student={'first_name':'yahya' ,'last_name': 'mannai','gender': "male",'age': 20 ,'maritial status':'married','skills':["python","php","html","css","javascript"],'country':'tunisia','city':'kef','address':'i dont know'}
+print("the lenth of the student dictionary is",len(student))
+print(student.get("skills"))
+print(type(student['skills']))
+#idk too lazy to finish may come back later
+keys=student.keys()
+print(keys)
+values=student.values()
+print(values)
+#yeah still lazy
